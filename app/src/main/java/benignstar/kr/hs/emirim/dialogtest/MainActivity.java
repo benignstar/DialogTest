96958458469ff3b4aa8334f44b3ec1e63b1bf8a8
benignstar <che0009@naver.com>
2017-05-25 20:16:50 +0900

    #1 MainActivity Layout, Event Programming

---
+++ app/src/main/java/benignstar/kr/hs/emirim/dialogtest/MainActivity.java
@@ -0,0 +1,27 @@
+package benignstar.kr.hs.emirim.dialogtest;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button but1=(Button) findViewById(R.id.but1);
+        but1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        AlertDialog.Builder dlg=new AlertDialog.Builder(, this);
+        dlg.setTitle("Title");
+        dlg.setMessage("Message");
+        dlg.setIcon(R.drawable.idea);
+        dlg.show();
+    }
+}
